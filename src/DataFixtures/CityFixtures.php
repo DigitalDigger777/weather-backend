bdd9d06ed567b33b98f886fa328991a051ef5c95
korman <korman.yuri@gmail.com>
2018-09-06 18:19:50 +0300

    fix kyiv name

--- src/DataFixtures/CityFixtures.php
+++ src/DataFixtures/CityFixtures.php
@@ -45,7 +45,7 @@ class CityFixtures extends Fixture
     private function cities()
     {
         return [
-            'Kiyv',
+            'Kyiv',
             'London',
             'Madrid'
         ];

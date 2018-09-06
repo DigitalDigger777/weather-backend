a3940f76aa5981c2d6a610a21e5ef9cce7113307
korman <korman.yuri@gmail.com>
2018-09-06 18:14:27 +0300

    fix url

--- src/Controller/WeatherJSONController.php
+++ src/Controller/WeatherJSONController.php
@@ -37,7 +37,7 @@ class WeatherJSONController extends AbstractController
         if ($city = $em->getRepository(City::class)->find($cityId)) {
 
             $client = new Client();
-            $response = $client->get('https://samples.openweathermap.org/data/2.5/forecast', [
+            $response = $client->get('http://api.openweathermap.org/data/2.5/weather', [
                 'query' => [
                     'q' => $city->getName(),
                     'APPID' => $appId
@@ -50,7 +50,7 @@ class WeatherJSONController extends AbstractController
                 $data = [
                     'weather_data' => $data,
                     'sys' => [
-                        'url' => 'api.openweathermap.org/data/2.5/weather?q=' . $city->getName() . '&appid=' . $appId
+                        'url' => 'http://api.openweathermap.org/data/2.5/weather?q=' . $city->getName() . '&appid=' . $appId
                     ]
                 ];
             } else {

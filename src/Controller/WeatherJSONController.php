73a9026a5acafd915a73d071285ee8688284b01a
korman <korman.yuri@gmail.com>
2018-09-06 16:55:07 +0300

    remove ssl verify

--- src/Controller/WeatherJSONController.php
+++ src/Controller/WeatherJSONController.php
@@ -50,7 +50,7 @@ class WeatherJSONController extends AbstractController
                 $data = [
                     'weather_data' => $data,
                     'sys' => [
-                        'url' => 'https://api.openweathermap.org/data/2.5/forecast?q=' . $city->getName() . '&appid=' . $appId
+                        'url' => 'http://api.openweathermap.org/data/2.5/forecast?q=' . $city->getName() . '&appid=' . $appId
                     ]
                 ];
             } else {
